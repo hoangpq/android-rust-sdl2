6d0cef64003b14a8fce4f64eca9e660bea2cfdd2
hoangpq <pquochoang2007@gmail.com>
2019-11-07 23:33:34 +0700

    update download dependencies

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-pushd external/SDL2
+cd external/SDL2
 
 wget https://www.libsdl.org/release/SDL2-2.0.8.zip
 unzip -q SDL2-2.0.8.zip
@@ -11,7 +11,3 @@ wget https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.3.zip
 unzip -q SDL2_image-2.0.3.zip
 mv SDL2_image-2.0.3 SDL2_image
 rm SDL2_image-2.0.3.zip
-
-popd
-# No need to copy SDLActivity.java et al, repo contains those from 2.0.8
-# cp external/SDL2/SDL2/android-project/app/src/main/java/org/libsdl/app/*.java android/app/src/main/java/org/libsdl/app/
